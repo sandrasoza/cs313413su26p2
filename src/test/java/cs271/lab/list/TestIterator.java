4d6e9d5a5f7033a760977d72e57cf5ef7d15203f
sandrasoza <ssozazambrano@luc.edu>
2026-06-10 00:21:37 -0500

    Completed unit testing on testRemove -Utilized assertEquals and ListOf to express the remaining values in the list.

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -78,9 +78,9 @@ public class TestIterator {
       }
     }
     // TODO using assertEquals and List.of, express which values are left in the list
+    assertEquals(List.of(33, 44, 55, 66), list);
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
-    fail("Not yet implemented"); // remove this line when done
   }
 
   @Test
