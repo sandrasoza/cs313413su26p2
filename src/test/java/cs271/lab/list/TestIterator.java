b09961cae5bf07ebea72cfcc00d24fd7cdd8f594
sandrasoza <ssozazambrano@luc.edu>
2026-06-10 00:39:48 -0500

    Fixed inline comments spacing.

--- src/test/java/cs271/lab/list/TestIterator.java
+++ src/test/java/cs271/lab/list/TestIterator.java
@@ -77,7 +77,6 @@ public class TestIterator {
         i.remove(); // TODO Question: What happens if you use list.remove(Integer.valueOf(77))?
       }
     }
-
     // TODO using assertEquals and List.of, express which values are left in the list
     // See TestList.java for examples of how to use List.of; also see the Java List
     // interface for more information
