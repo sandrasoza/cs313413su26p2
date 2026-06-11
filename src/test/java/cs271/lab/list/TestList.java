38fd26ddc723de715acdd7e6bd38b04e217f7821
sandrasoza <ssozazambrano@luc.edu>
2026-06-10 21:49:53 -0500

    completed testContains and fixed testAddMultiple assertions

--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -52,7 +52,9 @@ public class TestList {
     // TODO write assertions using
     // list.contains(77)
     // that hold before and after adding 77 to the list
-    fail("Not yet implemented"); // remove this line when done
+    assertEquals(false,list.contains(77)); //before adding
+    list.add(77);
+    assertEquals(true,list.contains(77));
   }
 
   @Test
@@ -61,10 +63,10 @@ public class TestList {
     list.add(77);
     list.add(77);
     // TODO fix the expected values in the assertions below
-    assertEquals(0, list.size());
+    assertEquals(3, list.size());
     assertEquals(0, list.indexOf(77));
-    assertEquals(0, list.get(1).intValue());
-    assertEquals(0, list.lastIndexOf(77));
+    assertEquals(77, list.get(1).intValue());
+    assertEquals(2, list.lastIndexOf(77));
   }
 
   @Test
