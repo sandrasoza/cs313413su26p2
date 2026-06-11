e076f2c1248ed1c729f012ba25a16aac12faa91e
sandrasoza <ssozazambrano@luc.edu>
2026-06-10 22:59:58 -0500

    fixed retainAll statement

--- src/test/java/cs271/lab/list/TestList.java
+++ src/test/java/cs271/lab/list/TestList.java
@@ -171,7 +171,7 @@ public class TestList {
     list.add(66);
     // TODO in a single statement using retainAll and List.of,
     // remove items from the list to make the following assertions pass
-    list.retainAll(List.of(77, 77, 77));
+    list.retainAll(List.of(77));
     // (without touching the assertions themselves)
     assertEquals(3, list.size());
     assertEquals(List.of(77, 77, 77), list);
